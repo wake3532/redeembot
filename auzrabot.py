00576e9f6300ebce016853ba95b50333d3ea6c28
wake3532 <70120646+wake3532@users.noreply.github.com>
2020-12-08 13:31:25 +0900

    Update auzrabot.py

--- auzrabot.py
+++ auzrabot.py
@@ -13,7 +13,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content == "!redeem 24263REDEEMNWTHENFG":
+    if message.content == "!redeem 46423hcFDLKEFSUJK":
         Image_captcha = ImageCaptcha()
         msg = ""
         a = ""
@@ -24,7 +24,7 @@ async def on_message(message):
         Image_captcha.write(a, name)
 
         await message.channel.send(file=discord.File(name))
-        embed = discord.Embed(title="좋아요. 좋은 구매에 선택입니다", description = message.author.mention + ", 당신은 봇이 아닙니까? 사람이 아님을 인증해주세요", timestamp=message.created_at,
+        embed = discord.Embed(title="좋아요. 좋은 구매에 선택입니다", description = message.author.mention + ", 매크로가 아님을 인증해주십시오 접두사를 빼고 입력하십시오", timestamp=message.created_at,
         colour=discord.Colour.blurple()
     )
         await message.channel.send(embed=embed)
@@ -41,7 +41,7 @@ async def on_message(message):
             await message.channel.send(embed=embed)
 
         if msg.content == a:
-            embed = discord.Embed(title="즐거운 젠 돼십시오", description = message.author.mention + "인증코드를 정확히 입력하셨습니다 축하합니다 \n\n젠을 사용하기전 젠에 규칙은 없지만 관리자가 선을 넘었다 생각하면 밴에 해당합니다 이것에 동의를 하십니까? ", timestamp=message.created_at,
+            embed = discord.Embed(title="즐거운 젠 돼십시오", description = message.author.mention + "인증코드를 정확히 입력하셨습니다 축하합니다 \n\n 잠시만요 젠을 사용하기전 너무 많이 뽑았다 생각하면 GENR 박탈입니다 이것에 동의하면 체크 반응을 해주십시오 ", timestamp=message.created_at,
             colour=discord.Colour.green()
     )
             m = await message.channel.send(embed=embed)
@@ -66,7 +66,7 @@ async def on_message(message):
                     await message.author.add_roles(role)
         
         else:
-                embed = discord.Embed(title="실패!", description = message.author.mention + ", __**Captcha**__ 인증코드가 올바르지 않아요! 다시 시도해봐요.", timestamp=message.created_at,
+                embed = discord.Embed(title="OOPS", description = message.author.mention + "인증코드가 틀립니다", timestamp=message.created_at,
                 colour=discord.Colour.red()
     )
                 await message.channel.send(embed=embed)
